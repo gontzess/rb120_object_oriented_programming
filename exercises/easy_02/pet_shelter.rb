41da860add94a6a3c9b7dbb881f1f46677089b27
Stephen Gontzes <gontzess@gmail.com>
2021-03-09 13:18:10 -0500

    exercises progress mar 09

---
+++ exercises/easy_02/pet_shelter.rb
@@ -0,0 +1,146 @@
+# Write the classes and methods that will be necessary to make this code run, and print the following output:
+# P Hanson has adopted the following pets:
+# a cat named Butterscotch
+# a cat named Pudding
+# a bearded dragon named Darwin
+#
+# B Holmes has adopted the following pets:
+# a dog named Molly
+# a parakeet named Sweetie Pie
+# a dog named Kennedy
+# a fish named Chester
+#
+# P Hanson has 3 adopted pets.
+# B Holmes has 4 adopted pets.
+
+
+class Pet
+  attr_reader :type, :name
+
+  def initialize(type, name)
+    @type = type
+    @name = name
+  end
+
+  def to_s
+    "a #{type} named #{name}"
+  end
+end
+
+class Owner
+  attr_reader :name, :pets
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+
+  def add_pet(pet)
+    @pets << pet
+  end
+
+  def number_of_pets
+    pets.length
+  end
+end
+
+class Shelter < Owner
+  def initialize(name)
+    super
+    @adoptions_tracker = {}
+  end
+
+  def adopt(owner, pet)
+    owner.add_pet(pet)
+    self.add_adoption_to_tracker(owner, pet)
+  end
+
+  def print_adoptions
+    @adoptions_tracker.each do |owner, pets|
+      puts "#{owner.name} has adopted the following pets:"
+      puts pets
+      puts ""
+    end
+  end
+
+  private
+
+  def add_adoption_to_tracker(new_owner, pet)
+    @adoptions_tracker[new_owner] ||= []
+    @adoptions_tracker[new_owner] << pet
+  end
+end
+
+# butterscotch = Pet.new('cat', 'Butterscotch')
+# pudding      = Pet.new('cat', 'Pudding')
+# darwin       = Pet.new('bearded dragon', 'Darwin')
+# kennedy      = Pet.new('dog', 'Kennedy')
+# sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+# molly        = Pet.new('dog', 'Molly')
+# chester      = Pet.new('fish', 'Chester')
+#
+# phanson = Owner.new('P Hanson')
+# bholmes = Owner.new('B Holmes')
+#
+# shelter = Shelter.new
+# shelter.adopt(phanson, butterscotch)
+# shelter.adopt(phanson, pudding)
+# shelter.adopt(phanson, darwin)
+# shelter.adopt(bholmes, kennedy)
+# shelter.adopt(bholmes, sweetie)
+# shelter.adopt(bholmes, molly)
+# shelter.adopt(bholmes, chester)
+# shelter.print_adoptions
+# puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+# puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+
+# ________
+# FURTHER EXPLORATION: Suppose the shelter has a number of not-yet-adopted pets, and wants to manage them through this same system. Thus, you should be able to add the following output to the example output shown above:
+# The Animal Shelter has the following unadopted pets:
+# a dog named Asta
+# a dog named Laddie
+# a cat named Fluffy
+# a cat named Kat
+# a cat named Ben
+# a parakeet named Chatterbox
+# a parakeet named Bluebell
+#    ...
+#
+# P Hanson has 3 adopted pets.
+# B Holmes has 4 adopted pets.
+# The Animal shelter has 7 unadopted pets.
+
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+astra        = Pet.new('dog', 'Astra')
+laddie       = Pet.new('dog', 'Laddie')
+fluffy       = Pet.new('cat', 'Fluffy')
+kat          = Pet.new('cat', 'Kat')
+ben          = Pet.new('cat', 'Ben')
+chatterbox   = Pet.new('parakeet', 'Chatterbox')
+bluebell     = Pet.new('parakeet', 'Bluebell')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+shelter = Shelter.new('Animal Shelter')
+
+unadopted_pets = [astra, laddie, fluffy, kat, ben, chatterbox, bluebell]
+unadopted_pets.each { |pet| shelter.adopt(shelter, pet) }
+
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+shelter.print_adoptions
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+puts "#{shelter.name} has #{shelter.number_of_pets} unadopted pets."
